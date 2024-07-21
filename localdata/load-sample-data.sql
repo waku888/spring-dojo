3302b3fa1ada63d194a931b4b55b754ecc97a889
waku <pleyel_waku@outlook.com>
2024-07-21 21:56:32 +0900

    02-19_ユーサー管理用のテーブルを作成する

--- localdata/load-sample-data.sql
+++ localdata/load-sample-data.sql
@@ -1,3 +1,5 @@
 INSERT INTO articles (title, body) VALUES ('タイトルです1','1本文です。');
 INSERT INTO articles (title, body) VALUES ('タイトルです2','2本文です。');
 INSERT INTO articles (title, body) VALUES ('タイトルです3','3本文です。');
+
+INSERT INTO users (username, password, enabled) VALUES ('user', 'password', true);
---
+++ src/main/resources/db/migration/V0.0.2__Create_users_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE users
+(
+    username VARCHAR(50) NOT NULL PRIMARY KEY,
+    password VARCHAR(500) NOT NULL,
+    enabled BOOLEAN NOT NULL
+);

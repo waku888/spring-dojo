650936fbf8cb3ae5a93de254345c06bf5d3738d6
waku <pleyel_waku@outlook.com>
2024-08-12 17:45:29 +0900

    03-11_@SpringBootTestを付与したテストクラスを作成

--- src/test/java/com/example/blog/BlogApplicationTests.java
+++ src/test/java/com/example/blog/BlogApplicationTests.java
@@ -1,13 +1,25 @@
 package com.example.blog;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class BlogApplicationTests {
 
+	@Autowired
+	private ApplicationContext applicationContext;
+
 	@Test
 	void contextLoads() {
+		// ## Arrange ##
+		var bean = applicationContext.getBean("userRestController");
+		// ## Act ##
+        // ## Assert ##
+		assertThat(bean).isNotNull();
 	}
 
 }

81a4134b0afb6d78730b40e31cc673479814eacc
waku <pleyel_waku@outlook.com>
2024-08-13 11:23:25 +0900

    03-16 IndexRestController に @RestController アノテーションが付与されていないことが原因を修正

--- src/main/java/com/example/blog/web/controller/IndexRestController.java
+++ src/main/java/com/example/blog/web/controller/IndexRestController.java
@@ -2,7 +2,9 @@ package com.example.blog.web.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class IndexRestController {
 
     // GET /

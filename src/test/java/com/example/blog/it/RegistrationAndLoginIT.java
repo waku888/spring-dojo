9fc3649c4fc8c833df50bf85fa5881a61fc8499e
waku <pleyel_waku@outlook.com>
2024-08-25 16:36:58 +0900

    03-18_ユーザー登録のテストを実装する

--- src/test/java/com/example/blog/it/RegistrationAndLoginIT.java
+++ src/test/java/com/example/blog/it/RegistrationAndLoginIT.java
@@ -3,6 +3,7 @@ package com.example.blog.it;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import java.util.Optional;
@@ -17,6 +18,27 @@ public class RegistrationAndLoginIT {
 
     @Test
     public void integrationTest() {
+//      ユーザー作成
+        var xsrfToken = getRoot();
+        register(xsrfToken);
+
+//      ログイン失敗
+//      Cookie にXSRF-TOKEN がない
+//      ヘッダーにX-XSRF-TOKEN がない
+//      Cookie のXSRF-TOKEN とヘッダーのX-XSRF-TOKEN の値が異なる
+//      ユーザー名が存在しない
+//      パスワードがデータベースに保存されているパスワードと違う
+//      ログイン成功
+//      ユーザー名がデータベースに存在する
+//      パスワードがデータベースに保存されているパスワードと違う
+//      Cookie のXSRF-TOKEN とヘッダーのX-XSRF-TOKEN の値が一致する
+//      → 200 OK が返る
+//      → レスポンスにSet-Cookie: JSESSIONID が返ってくる
+
+    }
+
+    private String getRoot() {
+
         // ## Arrange ##
 
         // ## Act ##
@@ -24,7 +46,7 @@ public class RegistrationAndLoginIT {
 
         // ## Assert ##
         var response = responseSpec.returnResult(String.class);
-        var xsrfTokenOpt =  Optional.ofNullable(response.getResponseCookies().getFirst("XSRF-TOKEN"));
+        var xsrfTokenOpt = Optional.ofNullable(response.getResponseCookies().getFirst("XSRF-TOKEN"));
 
         responseSpec.expectStatus().isNoContent();
         assertThat(xsrfTokenOpt)
@@ -32,7 +54,26 @@ public class RegistrationAndLoginIT {
                 .hasValueSatisfying(xsrfTokenCoolie ->
                         assertThat(xsrfTokenCoolie.getValue()).isNotBlank()
                 );
-        // Optional<null> isPresent()でチェック
-        // Optional<XSRF-TOKEN=aaaa;>
+        return xsrfTokenOpt.get().getValue();
+    }
+    private void register(String xsrfToken){
+        // ## Arrange ##
+        var bodyJson = """
+        {
+        "username": "user2",
+        "password": "password2"
+        }
+        """;
+        // ## Act ##
+        var responseSpec = webTestClient
+                .post().uri("/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .cookie("XSRF-TOKEN", xsrfToken)
+                .header("X-XSRF-TOKEN", xsrfToken)
+                .bodyValue(bodyJson)
+                .exchange();
+        // ## Assert ##
+        responseSpec.expectStatus().isCreated();
+
     }
 }

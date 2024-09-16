e82529e9066b30554036e84d4f22e84ac8c4a5f4
waku <pleyel_waku@outlook.com>
2024-09-16 14:15:00 +0900

    04-22_Serivce のテスト（モックあり）：findById が Optional<ArticleEntity> を返す場合

--- src/test/java/com/example/blog/service/article/ArticleServiceMockTest.java
+++ src/test/java/com/example/blog/service/article/ArticleServiceMockTest.java
@@ -1,12 +1,14 @@
 package com.example.blog.service.article;
 
 import com.example.blog.repository.article.ArticleRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -38,5 +40,34 @@ class ArticleServiceMockTest {
                         assertThat(article.id()).isEqualTo(999)
                 );
         assertThat(articleRepository.selectById(111)).isEmpty();
+    }
+
+
+    @Test
+    @DisplayName("findById: 指定されたIDの記事が存在するとき、ArticleEntityを返す")
+    public void findById_returnArticleEntity() {
+        // ## Arrange ##
+        var expected = new ArticleEntity(
+                999,
+                "title_999",
+                "body_999",
+                LocalDateTime.of(2022, 1, 1, 10, 0, 0),
+                LocalDateTime.of(2022, 2, 1, 11, 0, 0)
+        );
+        when(articleRepository.selectById(999)).thenReturn(Optional.of(expected));
+        // ## Act ##
+        var actual = cut.findById(999);
+        // ## Assert ##
+        assertThat(actual)
+                .isPresent()
+                .hasValueSatisfying(article -> {
+                    assertThat(article.id()).isEqualTo(999);
+                    assertThat(article.title()).isEqualTo("title_999");
+                    assertThat(article.content()).isEqualTo("body_999");
+                    assertThat(article.createdAt()).isEqualTo("2022-01-01T10:00:00");
+                    assertThat(article.updatedAt()).isEqualTo("2022-02-01T11:00:00");
+
+                });
+//                        assertThat(article).isEqualTo(expected));
     }
 }

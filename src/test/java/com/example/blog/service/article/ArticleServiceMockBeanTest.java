2d8a721d4d2b3fcfeb46544583bb8c6e7832453c
waku <pleyel_waku@outlook.com>
2024-09-16 14:34:52 +0900

    04-25_リファクタリング：テストに必要なコンポーネントだけをロードする

--- src/test/java/com/example/blog/service/article/ArticleServiceMockBeanTest.java
+++ src/test/java/com/example/blog/service/article/ArticleServiceMockBeanTest.java
@@ -3,9 +3,11 @@ package com.example.blog.service.article;
 import com.example.blog.repository.article.ArticleRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
@@ -13,7 +15,10 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
-@SpringBootTest
+//@SpringBootTest
+@MybatisTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE) //InMemorydbを使わずにMySqlを使うようにする
+@Import(ArticleService.class)
 class ArticleServiceMockBeanTest {
 
     @MockBean

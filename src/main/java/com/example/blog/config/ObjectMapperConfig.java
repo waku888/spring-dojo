5f0ca12d657f20433417a1c0ae9c1febed95c0e9
waku <pleyel_waku@outlook.com>
2024-09-16 16:19:48 +0900

    04-28_Controller のテスト（モックあり）：200 OK

--- src/main/java/com/example/blog/config/ObjectMapperConfig.java
+++ src/main/java/com/example/blog/config/ObjectMapperConfig.java
@@ -1,14 +1,25 @@
 package com.example.blog.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Configuration
 public class ObjectMapperConfig {
 
     @Bean
     public ObjectMapper objectMapper() {
-        return new ObjectMapper();
+        var timeModule = new JavaTimeModule();
+        timeModule.addSerializer(
+                LocalDateTime.class,
+                new LocalDateTimeSerializer(DateTimeFormatter.ISO_DATE_TIME)
+        );
+        return new ObjectMapper()
+                .registerModule(timeModule);
     }
 }
--- src/test/java/com/example/blog/web/controller/article/ArticleRestControllerMockTest.java
+++ src/test/java/com/example/blog/web/controller/article/ArticleRestControllerMockTest.java
@@ -1,23 +1,58 @@
 package com.example.blog.web.controller.article;
 
+import com.example.blog.config.ObjectMapperConfig;
+import com.example.blog.config.SecurityConfig;
+import com.example.blog.service.article.ArticleEntity;
 import com.example.blog.service.article.ArticleService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(ArticleRestController.class)
+@Import({ObjectMapperConfig.class, SecurityConfig.class})
 class ArticleRestControllerMockTest {
     @Autowired
     private MockMvc mockMvc;
 
     @MockBean
     private ArticleService mockArticleService;
+
+    @MockBean
+    private UserDetailsService userDetailsService;
+
     @Test
     public void mockMvc() {
         assertThat(mockMvc).isNotNull();
+    }
+
+    @Test
+    @DisplayName("GET /articles/{id}: 指定されたIDの記事が存在するとき、200 OK")
+    public void getArticlesById_200OK() throws Exception {
+        // ## Arrange ##
+        var expected = new ArticleEntity(
+                999,
+                "title_999",
+                "content_999",
+                LocalDateTime.of(2022, 1, 2, 3, 4, 5),
+                LocalDateTime.of(2023, 1, 2, 3, 4, 5)
+        );
+        when(mockArticleService.findById(expected.id())).thenReturn(Optional.of(expected));
+        // ## Act ##
+        var actual = mockMvc.perform(get("/articles/{id}", expected.id()));
+        // ## Assert ##
+        actual.andExpect(status().isOk());
     }
 }

a007c41d70bfa5addcb6c6fbf66fd631cf4714c1
waku <pleyel_waku@outlook.com>
2024-09-16 14:03:53 +0900

    04-21_Serivce のテスト：Mockito の練習

--- src/test/java/com/example/blog/service/article/ArticleServiceMockTest.java
+++ src/test/java/com/example/blog/service/article/ArticleServiceMockTest.java
@@ -7,7 +7,10 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -22,5 +25,18 @@ class ArticleServiceMockTest {
     @Test
     public void cut() {
         assertThat(cut).isNotNull();
+    }
+
+    @Test
+    public void mockPractice() {
+        when(articleRepository.selectById(999)).thenReturn(Optional.of(
+                new ArticleEntity(999, null, null, null, null)
+        ));
+        assertThat(articleRepository.selectById(999))
+                .isPresent()
+                .hasValueSatisfying(article ->
+                        assertThat(article.id()).isEqualTo(999)
+                );
+        assertThat(articleRepository.selectById(111)).isEmpty();
     }
 }

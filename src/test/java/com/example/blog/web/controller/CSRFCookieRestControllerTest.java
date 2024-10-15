1adcb7dedb0047a3769e0c8fca9f2f9ab84906cd
waku <pleyel_waku@outlook.com>
2024-10-15 15:12:41 +0900

    05-22_GET /csrf-cookie のテスト：@Nested を使ってリファクタリング する

--- src/test/java/com/example/blog/web/controller/CSRFCookieRestControllerTest.java
+++ src/test/java/com/example/blog/web/controller/CSRFCookieRestControllerTest.java
@@ -1,6 +1,7 @@
 package com.example.blog.web.controller;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -14,15 +15,20 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 class CSRFCookieRestControllerTest {
-    @Autowired
-    private MockMvc mockMvc;
 
-    @Test
-    @DisplayName("/csrf-cookie: GET リクエストを送ると、204 No Content が返る")
-    public void getCsrfCookie_return204() throws Exception {
-        mockMvc.perform(get("/csrf-cookie"))
-                .andExpect(status().isNoContent())
-                .andExpect(header().string("Set-Cookie",
-                        containsString("XSRF-TOKEN=")));
+    @Nested
+    class Return204Test {
+        @Autowired
+        private MockMvc mockMvc;
+
+        @Test
+        @DisplayName("/csrf-cookie: GET リクエストを送ると、204 No Content が返る")
+        public void getCsrfCookie_return204() throws Exception {
+            mockMvc.perform(get("/csrf-cookie"))
+                    .andExpect(status().isNoContent())
+                    .andExpect(header().string("Set-Cookie",
+                            containsString("XSRF-TOKEN=")));
+        }
+
     }
 }

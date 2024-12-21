41f38d211cc00691f81059f4dd5cfc97d1826707
waku <pleyel_waku@outlook.com>
2024-12-21 19:44:48 +0900

    06-22_UserRepository の修正_selectByUsername にnull が渡されたときSQL が実行されないようにする

--- src/main/java/com/example/blog/repository/user/UserRepository.java
+++ src/main/java/com/example/blog/repository/user/UserRepository.java
@@ -7,6 +7,12 @@ import java.util.Optional;
 
 @Mapper
 public interface UserRepository {
+
+    default Optional<UserEntity> selectByUsername(String username) {
+        return Optional.ofNullable(username)
+                .flatMap(this::selectByUsernameInternal);
+    }
+
     @Select("""
             SELECT
              id
@@ -16,7 +22,7 @@ public interface UserRepository {
             FROM users u
             WHERE u.username = #{username}
             """)
-    Optional<UserEntity> selectByUsername(@Param("username") String username);
+    Optional<UserEntity> selectByUsernameInternal(@Param("username") String username);
 
     @Insert("""
             INSERT INTO users(username, password, enabled)

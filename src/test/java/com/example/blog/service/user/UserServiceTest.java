0b6d5cac401ade155a030878ffce81b932734c8e
waku <pleyel_waku@outlook.com>
2024-12-22 10:21:30 +0900

    06-27_ユーザー登録_UserService のテストを書く_テスト時に必要な 設定だけをロードするように工夫する

--- src/test/java/com/example/blog/service/user/UserServiceTest.java
+++ src/test/java/com/example/blog/service/user/UserServiceTest.java
@@ -1,29 +1,43 @@
 package com.example.blog.service.user;
 
+import com.example.blog.config.MybatisDefaultDatasourceTest;
+import com.example.blog.config.PasswordEncoderConfig;
 import com.example.blog.repository.user.UserRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Import;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-@SpringBootTest
-@Transactional
+@MybatisDefaultDatasourceTest
+@Import({UserService.class, PasswordEncoderConfig.class})
 class UserServiceTest {
 
     @Autowired
     private UserService cut;
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private ApplicationContext ctx;
 
     @Test
     void successAutowired() {
         assertThat(cut).isNotNull();
         assertThat(userRepository).isNotNull();
+        System.out.println("bean.length = " + ctx.getBeanDefinitionNames().length);
     }
 
+    @Test
+    @DisplayName("test description")
+    void method_success() {
+        // ## Arrange ##
+
+        // ## Act ##
+        // ## Assert ##
+    }
+    
     @Test
     @DisplayName("register: パスワードがハッシュ化されてデータベースに登録される")
     void register_success() {

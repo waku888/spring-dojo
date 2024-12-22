8a21acaade6775e2ba54b43565c83537d2dc524c
waku <pleyel_waku@outlook.com>
2024-12-22 14:38:51 +0900

    06-30_ユーザー登録_UserController のテストを書く_CsrfRequestPostProcessor を使って403 エラーを回避する

--- src/test/java/com/example/blog/web/controller/user/UserRestControllerTest.java
+++ src/test/java/com/example/blog/web/controller/user/UserRestControllerTest.java
@@ -5,17 +5,22 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Transactional
 class UserRestControllerTest {
     private static final String MOCK_USERNAME = "user1";
 
@@ -50,4 +55,23 @@ class UserRestControllerTest {
         actual.andExpect(status().isForbidden());
     }
 
+    @Test
+    @DisplayName("POST /users：ユーザー作成に成功すると、レスポンスボディにユーザ情報/LocationにURIがセットされる")
+    void createUser_success() throws Exception {
+        // ## Arrange ##
+        String newUserJson = """
+                    {
+                    "username": "username123",
+                    "password": "password123"
+                    }
+                    """;
+        // ## Act ##
+        var actual =mockMvc.perform(post("/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .with(csrf())
+                .content(newUserJson));
+        // ## Assert ##
+        actual.andExpect(status().isCreated());
+    }
+    
 }

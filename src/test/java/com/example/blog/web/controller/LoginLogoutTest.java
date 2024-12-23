d6ccd05d2d588bc0a0addf84959c559965f0f9d8
waku <pleyel_waku@outlook.com>
2024-12-23 16:12:25 +0900

    06-74_ログイン_MockMvcと使ったテストの準備をする

---
+++ src/test/java/com/example/blog/web/controller/LoginLogoutTest.java
@@ -0,0 +1,21 @@
+package com.example.blog.web.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class LoginLogoutTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @Test
+    void mockMvc() {
+        assertThat(mockMvc).isNotNull();
+    }
+}

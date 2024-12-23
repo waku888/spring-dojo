88510b04cc54b8c228114142299db8c8b4988616
waku <pleyel_waku@outlook.com>
2024-12-23 16:32:06 +0900

    06-75_ログイン_ログイン成功のテストを書く

--- src/test/java/com/example/blog/web/controller/LoginLogoutTest.java
+++ src/test/java/com/example/blog/web/controller/LoginLogoutTest.java
@@ -1,12 +1,18 @@
 package com.example.blog.web.controller;
 
+import com.example.blog.service.user.UserService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @SpringBootTest
@@ -14,8 +20,38 @@ import static org.assertj.core.api.Assertions.assertThat;
 class LoginLogoutTest {
     @Autowired
     private MockMvc mockMvc;
+    @Autowired
+    private UserService userService;
+
     @Test
     void mockMvc() {
         assertThat(mockMvc).isNotNull();
+    }
+
+    @Test
+    @DisplayName("POST /login: ログイン成功")
+    void login_success() throws Exception {
+        // ## Arrange ##
+        var username = "username123";
+        var password = "password123";
+        userService.register(username, password);
+        String newUserJson = """
+                    {
+                    "username": "%s",
+                    "password": "%s"
+                    }
+                    """.formatted(username, password);
+
+        // ## Act ##
+        var actual =mockMvc.perform(post("/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(newUserJson)
+                .with(csrf())
+        );
+
+        // ## Assert ##
+        actual.andExpect(status().isOk())
+        
+        ;
     }
 }

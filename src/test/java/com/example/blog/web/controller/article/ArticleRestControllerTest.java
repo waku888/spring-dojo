2a8d032ff5b7e053f11b0a75af127372b2f9cd39
waku <pleyel_waku@outlook.com>
2024-12-28 11:34:12 +0900

    07-06_POST /articles：テストクラスを準備する

---
+++ src/test/java/com/example/blog/web/controller/article/ArticleRestControllerTest.java
@@ -0,0 +1,33 @@
+package com.example.blog.web.controller.article;
+
+import com.example.blog.service.user.UserService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+class ArticleRestControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private UserService userService;
+
+    @Test
+    void setup() {
+        // ## Arrange ##
+
+        // ## Act ##
+
+        // ## Assert ##
+        assertThat(mockMvc).isNotNull();
+        assertThat(userService).isNotNull();
+    }
+}

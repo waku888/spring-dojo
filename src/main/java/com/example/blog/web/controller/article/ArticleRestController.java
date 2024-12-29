8ab6edd53e302073a6a6cae7652fca15e0dbccbd
waku <pleyel_waku@outlook.com>
2024-12-29 18:57:10 +0900

    07-21_POST /articles：レスポンスボディを実装する

--- src/main/java/com/example/blog/web/controller/article/ArticleRestController.java
+++ src/main/java/com/example/blog/web/controller/article/ArticleRestController.java
@@ -1,39 +1,40 @@
 package com.example.blog.web.controller.article;
 
 import com.example.blog.api.ArticlesApi;
+import com.example.blog.model.ArticleDTO;
+import com.example.blog.model.UserDTO;
 import com.example.blog.service.article.ArticleService;
-import com.example.blog.web.exception.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
+import java.time.OffsetDateTime;
 
 @RestController
 @RequiredArgsConstructor
 
 public class ArticleRestController implements ArticlesApi {
+
     private final ArticleService articleService;
 
-//    public ArticleRestController(ArticleService articleService){
-//        this.articleService = articleService;
-//    }
-    // GET /articles/1
-    @GetMapping("/articles/{id}")
-    public ArticleDTO showArticle(@PathVariable("id") long id){
-        return articleService.findById(id) // Optional<ArticleEntity>
-                .map(ArticleDTO::from)//Optional<ArticleDTO>
-                .orElseThrow(ResourceNotFoundException::new);
-    }
-
     @Override
-    public ResponseEntity<Void> createArticle() {
+    public ResponseEntity<ArticleDTO> createArticle() {
+
+        var user = new UserDTO();
+        user.setId(99L);
+        user.setUsername("test_user");
+        var body = new ArticleDTO();
+        body.setId(123L);
+        body.setTitle("test_title");
+        body.setBody("test_body");
+        body.setAuthor(user);
+        body.setCreatedAt(OffsetDateTime.now());
+        body.setUpdatedAt(OffsetDateTime.now());
         return ResponseEntity
                 .created(URI.create("/articles/123")) // TODO mock impl
                 .contentType(MediaType.APPLICATION_JSON)
-                .build();
+                .body(body);
     }
 }

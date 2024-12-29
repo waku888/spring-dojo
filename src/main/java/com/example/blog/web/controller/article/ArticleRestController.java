fb8a53452dd6e25c4da5d6d40acf3694560f6b37
waku <pleyel_waku@outlook.com>
2024-12-29 21:46:03 +0900

    07-26_POST /articles：ハンドラーメソッドでLoggedInUser を取得す る

--- src/main/java/com/example/blog/web/controller/article/ArticleRestController.java
+++ src/main/java/com/example/blog/web/controller/article/ArticleRestController.java
@@ -4,10 +4,12 @@ import com.example.blog.api.ArticlesApi;
 import com.example.blog.model.ArticleDTO;
 import com.example.blog.model.ArticleForm;
 import com.example.blog.model.UserDTO;
+import com.example.blog.security.LoggedInUser;
 import com.example.blog.service.article.ArticleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
@@ -15,17 +17,19 @@ import java.time.OffsetDateTime;
 
 @RestController
 @RequiredArgsConstructor
-
 public class ArticleRestController implements ArticlesApi {
 
     private final ArticleService articleService;
 
     @Override
     public ResponseEntity<ArticleDTO> createArticle(ArticleForm form) {
-
+        var loggedInUser = (LoggedInUser) SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getPrincipal();
         var userDTO = new UserDTO();
-        userDTO.setId(99L);
-        userDTO.setUsername("test_user");
+        userDTO.setId(loggedInUser.getUserId());
+        userDTO.setUsername(loggedInUser.getUsername());
         var body = new ArticleDTO();
         body.setId(123L);
         body.setTitle(form.getTitle());

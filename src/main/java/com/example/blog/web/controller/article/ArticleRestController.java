e3b817e3b81e3a470b63d8f08b63443bde0a3ccb
waku <pleyel_waku@outlook.com>
2024-12-29 19:16:37 +0900

    07-23_POST /articles：リクエストボディをハンドラメソッドで受け取る

--- src/main/java/com/example/blog/web/controller/article/ArticleRestController.java
+++ src/main/java/com/example/blog/web/controller/article/ArticleRestController.java
@@ -2,6 +2,7 @@ package com.example.blog.web.controller.article;
 
 import com.example.blog.api.ArticlesApi;
 import com.example.blog.model.ArticleDTO;
+import com.example.blog.model.ArticleForm;
 import com.example.blog.model.UserDTO;
 import com.example.blog.service.article.ArticleService;
 import lombok.RequiredArgsConstructor;
@@ -20,16 +21,16 @@ public class ArticleRestController implements ArticlesApi {
     private final ArticleService articleService;
 
     @Override
-    public ResponseEntity<ArticleDTO> createArticle() {
+    public ResponseEntity<ArticleDTO> createArticle(ArticleForm form) {
 
-        var user = new UserDTO();
-        user.setId(99L);
-        user.setUsername("test_user");
+        var userDTO = new UserDTO();
+        userDTO.setId(99L);
+        userDTO.setUsername("test_user");
         var body = new ArticleDTO();
         body.setId(123L);
-        body.setTitle("test_title");
-        body.setBody("test_body");
-        body.setAuthor(user);
+        body.setTitle(form.getTitle());
+        body.setBody(form.getBody());
+        body.setAuthor(userDTO);
         body.setCreatedAt(OffsetDateTime.now());
         body.setUpdatedAt(OffsetDateTime.now());
         return ResponseEntity

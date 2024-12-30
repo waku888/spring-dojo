4d9e0c01e72eb6ab5d68c1bd132e112a020bf352
waku <pleyel_waku@outlook.com>
2024-12-30 15:35:00 +0900

    07-32_POST /articles：データベース変更_articles とusers を紐づけ る

--- src/main/resources/db/migration_dev/R__Insert_articles.sql
+++ src/main/resources/db/migration_dev/R__Insert_articles.sql
@@ -1,13 +1,3 @@
-DELETE FROM articles;
-
-ALTER TABLE articles AUTO_INCREMENT = 1;
-
-INSERT INTO articles (title, body)
-VALUES ('タイトルです1', '1本文です。')
-     , ('タイトルです2', '2本文です。')
-     , ('タイトルです3', '3本文です。')
-;
-
 DELETE FROM users;
 
 ALTER TABLE articles AUTO_INCREMENT = 1;
@@ -18,3 +8,14 @@ VALUES (1, 'user1', '$2a$10$nyJuFN8ukeszAPUBELCxauSxU2fHygybh3.keJnq3mwEgai3Aulb
      , (2, 'user2', '$2a$10$8yF984DRLex7UDeCOG230ehnSVPm4OShzVVOYujPYcJsXPcy39czy', true)
      , (3, 'user3', '$2a$10$l8gXDAKbsGtUQhdrwE06N.ip6iNW4tauQW40Q6nhU8DMYF1sLmFvy', true)
 ;
+
+DELETE FROM articles;
+
+ALTER TABLE articles AUTO_INCREMENT = 1;
+
+INSERT INTO articles (title, body, user_id)
+VALUES ('タイトルです1', '1本文です。', 1)
+     , ('タイトルです2', '2本文です。', 1)
+     , ('タイトルです3', '3本文です。', 2)
+;
+

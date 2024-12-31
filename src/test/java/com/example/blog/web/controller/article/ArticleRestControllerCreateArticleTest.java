bd2a4818519e534a779aae1f694f1ab294907a7e
waku <pleyel_waku@outlook.com>
2024-12-31 15:12:13 +0900

    07-49_GET /articles：200 OK のController のテストクラスを用意す る

--- src/test/java/com/example/blog/web/controller/article/ArticleRestControllerCreateArticleTest.java
+++ src/test/java/com/example/blog/web/controller/article/ArticleRestControllerCreateArticleTest.java
@@ -22,7 +22,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
-class ArticleRestControllerTest {
+class ArticleRestControllerCreateArticleTest {
 
     @Autowired
     private MockMvc mockMvc;

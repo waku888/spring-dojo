cebb77adff57ee68b7337f899bf36f9817aa547b
waku <pleyel_waku@outlook.com>
2024-12-31 21:05:49 +0900

    07-55_GET /articles：OffsetDatetime のJSON 出力を調整する

--- src/main/java/com/example/blog/config/ObjectMapperConfig.java
+++ src/main/java/com/example/blog/config/ObjectMapperConfig.java
@@ -1,25 +1,19 @@
 package com.example.blog.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 @Configuration
 public class ObjectMapperConfig {
 
     @Bean
     public ObjectMapper objectMapper() {
-        var timeModule = new JavaTimeModule();
-        timeModule.addSerializer(
-                LocalDateTime.class,
-                new LocalDateTimeSerializer(DateTimeFormatter.ISO_DATE_TIME)
-        );
         return new ObjectMapper()
-                .registerModule(timeModule);
+                .registerModule(new JavaTimeModule())
+                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+                ;
     }
 }

c8ab14dadcf8141debc2b4ad62fee015da31f9af
waku <pleyel_waku@outlook.com>
2025-02-15 21:20:19 +0900

    08-48_DELETE /articles/{id}：Repository 層のテストを追加する_著者ID不一致

--- src/test/java/com/example/blog/repository/article/ArticleRepositoryTest.java
+++ src/test/java/com/example/blog/repository/article/ArticleRepositoryTest.java
@@ -323,4 +323,40 @@ class ArticleRepositoryTest {
                     .isEqualTo(existingArticle);
         });
     }
+    @Test
+    @DisplayName("delete: 著者以外のユーザーIDが指定されているとき、削除しない")
+    void delete_invalidAuthorId() {
+        // ## Arrange ##
+        var author = new UserEntity(null, "test_username1", "test_password1", true);
+        userRepository.insert(author);
+        var otherUser = new UserEntity(null, "test_username2", "test_password2", true);
+        userRepository.insert(otherUser);
+        var existingArticle = new ArticleEntity(
+                null,
+                "test_title",
+                "test_body",
+                author,
+                TestDateTimeUtil.of(2020, 1, 10, 10, 20, 30),
+                TestDateTimeUtil.of(2020, 1, 10, 10, 20, 30)
+        );
+        cut.insert(existingArticle);
+        var articleToDelete = new ArticleEntity(
+                existingArticle.getId(),
+                existingArticle.getTitle(),
+                existingArticle.getBody(),
+                otherUser,
+                existingArticle.getCreatedAt(),
+                existingArticle.getUpdatedAt()
+        );
+        // ## Act ##
+        cut.update(articleToDelete);
+        // ## Assert ##
+        var actual = cut.selectById(existingArticle.getId());
+        assertThat(actual).hasValueSatisfying(actualArticle -> {
+            assertThat(actualArticle)
+                    .usingRecursiveComparison()
+                    .ignoringFields("author.password")
+                    .isEqualTo(existingArticle);
+        });
+    }
 }

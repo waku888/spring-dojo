2136b34f51fbac17ba4c7ca329739b04300accac
waku <pleyel_waku@outlook.com>
2025-02-16 22:10:04 +0900

    09-18_POST comment_404 NotFound：テストを書く

--- src/test/java/com/example/blog/web/controller/article/ArticleRestControllerCreateArticleCommentTest.java
+++ src/test/java/com/example/blog/web/controller/article/ArticleRestControllerCreateArticleCommentTest.java
@@ -193,4 +193,33 @@ class ArticleRestControllerCreateArticleCommentTest {
                 ))
         ;
     }
+
+    @Test
+    @DisplayName("POST /articles/{articleId}/comments: 指定されたIDの記事が存在しないとき、404を返す")
+    void postArticleComments_404NotFound() throws Exception {
+        // ## Arrange ##
+        var invalidArticleId = 0;
+        var bodyJson = """
+                {
+                "body": "これはテストのコメントです"
+                }
+                """;
+        // ## Act ##
+        var actual = mockMvc.perform(
+                post("/articles/{articleId}/comments", invalidArticleId)
+                        .with(csrf())
+                        .with(user(loggedInCommentAuthor))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(bodyJson)
+        );
+        // ## Assert ##
+        actual
+                 .andExpect(status().isNotFound())
+                .andExpect(content().contentType(MediaType.APPLICATION_PROBLEM_JSON))
+                .andExpect(jsonPath("$.title").value("Not Found"))
+                .andExpect(jsonPath("$.status").value(404))
+                .andExpect(jsonPath("$.detail").value("リソースが見つかりません"))
+                .andExpect(jsonPath("$.instance").value("/articles/%d/comments".formatted(invalidArticleId)))
+        ;
+    }
 }
